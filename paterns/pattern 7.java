28d29bbc1cf78b32eb0e30b2cefbb13b8830d43c
umirbek1307 <46290887+umirbek1307@users.noreply.github.com>
2020-07-21 08:19:29 +0530

    Add files via upload

---
+++ paterns/pattern 7.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    System.out.print("*\t");
+                } else if(i + j== n + 1) {
+                    System.out.print("*\t");
+                }
+                else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
